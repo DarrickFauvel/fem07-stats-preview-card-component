843f5fe6cd6fc799cb359d34d2d245dd60664ea6
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-02-22 19:26:43 -0500

    Add height offset script

---
+++ dist/index.09c24910.js
@@ -0,0 +1,5 @@
+const mainEl = document.querySelector('main');
+const footerEl = document.querySelector('footer');
+mainEl.style.marginTop = footerEl.offsetHeight + 'px';
+
+//# sourceMappingURL=index.09c24910.js.map
---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+const mainEl = document.querySelector('main')
+const footerEl = document.querySelector('footer')
+
+mainEl.style.marginTop = footerEl.offsetHeight + 'px'
